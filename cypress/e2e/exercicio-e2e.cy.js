aadfdbe14734745121d614fb5fa7855617f2a3e9
João Zurobski <joaozurobski@gmail.com>
2024-02-26 20:39:30 -0300

    Entrega Exercicio Modulo 12

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,50 @@
 /// <reference types="cypress" />
 
+const { faker } = require('@faker-js/faker');
+import checkout from '../support/page_objects/checkout';
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
-
-  beforeEach(() => {
-      cy.visit('/')
-  });
-
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
 
 
-})
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
+
+        
+    beforeEach(() => {
+        cy.visit('produtos/')
+    });
+
+    it('Realizando a compra de 4 produtos', () => {
+        cy.addProdutos('Ajax Full-Zip Sweatshirt', 'S', 'Green', '1')
+        cy.visit('produtos/')
+        cy.addProdutos('Apollo Running Short', '32', 'Black', '1')
+        cy.visit('produtos/')
+        cy.addProdutos('Atlas Fitness Tank', 'XL', 'Blue', '1')
+        cy.visit('produtos/')
+        cy.addProdutos('Argus All-Weather Tank', 'L', 'Gray', '1')
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
+        cy.get('.checkout-button').click()
+
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let empresaFaker = faker.company.name()
+        let endereco = faker.address.streetAddress()
+        let enderecoNumero = faker.address.buildingNumber()
+        let fone = faker.number.int()
+        let email = faker.internet.email()
+
+        checkout.editarInformacoesCheckout(nomeFaker, sobrenomeFaker, empresaFaker, 'Brasil', endereco, enderecoNumero, 'Sao Paulo', 'Sao Paulo', '11111111', fone, email)
+    });
+
+});
+
+
+
+
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,17 @@
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
+});
+Cypress.Commands.add('addProdutos', (produtos, tamanho, cor, quantidade) => {
+    cy.get('[class="product-block grid"]')
+        .contains(produtos)
+        .click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.woocommerce-message > .button')
+        .click()
 });
 
